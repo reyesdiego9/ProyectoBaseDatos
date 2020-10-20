d1c4cd1c00b2c7487756bf27aa193c12ecdd4e1a
JordiGon <jgonzalezq2@miumg.edu.gt>
2020-10-19 21:00:29 -0600

    Agregue mi conexion a cada formulario

--- prueba.php
+++ prueba.php
@@ -1,6 +1,6 @@
 <?php   
 $sql = "SELECT * FROM PROD WHERE ID_PRODUCTO = ". (int) $_GET['id'];
-$conn = oci_connect("diegopapi", "toor", "localhost:1521/xe");
+$conn = oci_connect("jordi", "clave", "localhost:1521/xe");
 $stid = oci_parse($conn, $sql);
 oci_execute($stid);
 $row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS);
@@ -8,7 +8,7 @@ if (!$row) {
        header('Status: 404 Not Found');
 } else {
        $img = $row['IMAGEN']->load();
-       header("Content-type: image/jpeg");
+       header("Content-type: image/*");
        print $img;
 }
 ?>
